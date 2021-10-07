0e3680f165e1b3e9477fb1b1f19cd0ce76e2d3bc
riteshrky <91357648+riteshrky@users.noreply.github.com>
2021-10-07 10:11:53 +0530

    PYTHON DAY 1
    DATA TYPE , INBUILT FUNCTION , BOOLEAN

---
+++ DAY_1.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## DATA TYPE
+# string, Integer,boolean,float-single value Data type
+# Lists,sets,Tuples,Dictionary-Collection of Values (data structures)
+# 
+
+# ##Intengers
+# Arithmetic operaters
+
+# In[1]:
+
+
+a=7
+b=5
+
+
+# In[2]:
+
+
+a*b #multiply
+
+
+# In[3]:
+
+
+a/b#division
+
+
+# In[4]:
+
+
+a+b #addition
+
+
+# In[5]:
+
+
+a-b#substraction
+
+
+# In[6]:
+
+
+a%b#module
+
+
+# In[7]:
+
+
+a**2#for raised power
+
+
+# In[8]:
+
+
+a**b #raised power
+
+
+# In[9]:
+
+
+((a+b)*(a-b))#Bodmas
+
+
+# In[10]:
+
+
+a//b #floor division
+
+
+# ## inbuit function type
+# 
+
+# In[11]:
+
+
+type(a)
+
+
+# In[12]:
+
+
+type('python')
+
+
+# In[13]:
+
+
+type([1,2,3,4])
+
+
+# In[14]:
+
+
+type((1,2,3,4))
+
+
+# In[15]:
+
+
+type(True)
+
+
+# In[16]:
+
+
+first_name='Ritesh'
+Last_name='Gupta'
+
+
+# In[20]:
+
+
+print("My first name is {} and last name is {}".format(first_name,Last_name))
+
+
+# In[21]:
+
+
+help(round)##help
+
+
+# In[22]:
+
+
+len(first_name)
+
+
+# In[23]:
+
+
+type('3')
+
+
+# In[24]:
+
+
+first_name.capitalize
+
+
+# In[26]:
+
+
+print(first_name.capitalize())
+print(first_name.isalnum())
+print(first_name.isalpha())
+print(first_name.isnumeric())
+
+
+# ## Boolean Operators
+
+# In[27]:
+
+
+True and True 
+
+
+# In[28]:
+
+
+True and False
+
+
+# In[29]:
+
+
+True or False
+
+
+# In[30]:
+
+
+False and False
+
+
+# ##float
+
+# In[31]:
+
+
+a=7.9
+
+
+# In[32]:
+
+
+type(a)
+
+
+# In[ ]:
+
+
+
+
